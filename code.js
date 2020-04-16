84cd19ab484595841ebb6303c1e729f297130315
cristiano <criscaverzan8@gmail.com>
2020-04-16 16:21:39 -0300

    checagem se usuario trabalha com CSS

--- code.js
+++ code.js
@@ -1,9 +1,17 @@
+function usuarioTrabalhaComCSS(usuario){
+    var trabalha_com_css = usuario.tecnologias[0] == "CSS" | usuario.tecnologias[1] == "CSS"
+    return trabalha_com_css
+}
+
 const usuarios = [
     {nome:"Marcos", tecnologias:["HTML", "CSS"]},
-    {nome:"Tadeu", tecnologias:["PHP", "Laravel"]},
+    {nome:"Tadeu", tecnologias:["CSS", "Laravel"]},
     {nome:"Theomars", tecnologias:["C++", "Assembly"]}
 ]
 
-for (var i=0;i<usuarios.length;i++){
-    console.log(`${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias[0]} e ${usuarios[i].tecnologias[1]}`);
+for(var i=0;i<usuarios.length;i++){
+    var trabalha_com_CSS = usuarioTrabalhaComCSS(usuarios[i])
+    if (trabalha_com_CSS){
+        console.log(`${usuarios[i].nome} trabalha com CSS`);
+    }
 }
